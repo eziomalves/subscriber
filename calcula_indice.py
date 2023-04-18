c245fdbdc2869023b3daccae56ad0cdb323a3a38
Eziom Alves <eziomalves@hotmail.com>
2023-04-18 16:39:44 -0300

    Delete calcula_indice.py

--- calcula_indice.py
+++
@@ -1,7 +0,0 @@
-import bd
-from datetime import datetime
-
-#programa utilizado para calcular o indice a partir de uma data específica
-data = datetime(2023,3,15)
-print("Data calcula: " + str(data.date()))
-bd.salvar_indice(data.date())
